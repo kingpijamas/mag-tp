d00fe5a2420d1a29d305f5ccebfe658273875b29
Juan Pablo Rey <juanpablorey@restorando.com>
2016-11-28 19:05:16 -0300

    Restart button eyecandy

--- src/main/webapp/js/uiControls.js
+++ src/main/webapp/js/uiControls.js
@@ -1,6 +1,8 @@
 $(() => {
-    $('#restart-btn').click(() => {
+    var restartButton = $('#restart-btn');
+    restartButton.click(() => {
       resetCounters();
+      restartButton.text('Reiniciar');
       $.post('/restart');
     });
     $('#stop-btn').click(() => $.post('/stop'));
