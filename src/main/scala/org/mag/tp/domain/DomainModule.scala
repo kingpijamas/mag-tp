b727503a03dd6179bc7fb15c1be7f5c34a9c7422
kpjjpk <juanpabloreyv@gmail.com>
2016-11-28 21:42:57 -0300

    Cleanup

--- src/main/scala/org/mag/tp/domain/DomainModule.scala
+++ src/main/scala/org/mag/tp/domain/DomainModule.scala
@@ -12,22 +12,23 @@ import scala.language.postfixOps
 import scala.util.Random
 
 trait DomainModule {
-  val memory = None.taggedWith[MemorySize]
   val employeeTimerFreq = (0.1 seconds).taggedWith[Employee.TimerFreq]
 
   val targetEmployeeCount = 1000.taggedWith[EmployeeCount]
-  val broadcastability = 5.taggedWith[Broadcastability]
 
-  val workingProportion = 0.50
+  val memory = Some(1).taggedWith[MemorySize]
+  val broadcastability = 5.taggedWith[Broadcastability]
+  val workingProportionAtStart = 0.90
+  val workersPermeabilityAtStart = -0.3
 
   def employeePropsFactory(workArea: ActorRef @@ WorkArea): Props @@ Employee = {
     def permeabilityAndBehaviours(permeability: Double, behaviourProbs: (Behaviour, Double)*) =
       (permeability.taggedWith[Permeability], ProbabilityBag.complete[Employee.Behaviour](behaviourProbs: _*))
 
-    val (permeability, behaviour) = if (Random.nextDouble < workingProportion)
-      permeabilityAndBehaviours(0.01, WorkBehaviour -> 0, LoiterBehaviour -> 1)
+    val (permeability, behaviour) = if (Random.nextDouble < workingProportionAtStart)
+      permeabilityAndBehaviours(workersPermeabilityAtStart, WorkBehaviour -> 1, LoiterBehaviour -> 0)
     else
-      permeabilityAndBehaviours(0, WorkBehaviour -> 1, LoiterBehaviour -> 0)
+      permeabilityAndBehaviours(0, WorkBehaviour -> 0, LoiterBehaviour -> 1)
 
     Props(wire[Employee]).taggedWith[Employee]
   }
